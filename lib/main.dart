02494d8196dde6ceeeb48a7da28332f344ad1dd2
Kiran Johns <kiranjohns369@gmail.com>
2020-03-23 19:24:04 +0530

    cards

--- lib/main.dart
+++ lib/main.dart
@@ -42,37 +42,72 @@ class Home extends StatelessWidget {
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(left:50, right: 30, top:20),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Text(
-            'Hello Goku!',
-            style: TextStyle(
-              fontWeight:FontWeight.bold,
-              fontSize: 20,
-              letterSpacing: 1,
-            ) ,
-          ),
-          SizedBox(height: 5,),
-          Container(
-            color: Colors.white,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-              Text(
-                'Today',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color:Colors.grey,
+    return SingleChildScrollView(
+          child: Container(
+        margin: EdgeInsets.only(left:50, right: 30, top:20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              'Hello Goku!',
+              style: TextStyle(
+                fontWeight:FontWeight.bold,
+                fontSize: 20,
+                letterSpacing: 1,
+              ) ,
+            ),
+            SizedBox(height: 10,),
+            Container(
+              color: Colors.white,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                Text(
+                  'Today',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color:Colors.grey,
+                  ),
                 ),
-              ),
-              Icon(Icons.create, color: Colors.grey,)
-            ],),
-          )
-        ],
-      )
+                Icon(Icons.create, color: Colors.grey,)
+              ],),
+            ),
+            SizedBox(height:30 ),
+            Container(
+              child: Row(children: <Widget>[
+                Text(
+                  'Overall',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Icon(Icons.keyboard_arrow_down, color: Colors.grey,)
+              ],),
+            ),
+            Container(
+              height: 150,
+              child: Card(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children:<Widget>[
+                          CircleAvatar(
+                            radius: 25,
+                            child: ClipOval(child: Image.asset('assets/goku.png'),)
+                          ),
+                          Text("Goku"),
+                          Icon(Icons.collections),
+                          Column(children: <Widget>[
+                            
+                          ],)
+                        ]
+                      ),
+                      elevation: 2,
+                      
+                    ),
+            )
+          ],
+        )
+      ),
     );
   }
 }
